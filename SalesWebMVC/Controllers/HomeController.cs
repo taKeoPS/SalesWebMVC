9a6eb0860c899e0ca2c47ac4abd4fb89328a9526
Philipe Takeo Shima <philipe.shima@gmail.com>
2021-12-18 16:25:46 -0300

    Teste razor

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sistema Web de vendas.";
             ViewData["Email"] = "philipe.shima@gmail.com";
 
             return View();
