5d1e7fff35645252d167c64975ba81320ace9123
Philipe Takeo Shima <philipe.shima@gmail.com>
2021-12-24 12:49:37 -0300

    Foreign Key - Chave estrangeira não nulla. (integridade referencial)
    Drop DataBAse...

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -26,6 +26,7 @@ namespace SalesWebMVC.Services
         // Método para inserir o cadastro no banco de dados, referente a página de criar novo/vendedores
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj); // Só esta ação não confirma, apenas insere no bd
             _context.SaveChanges(); // Esta ação confirma a alteração acima
         }
